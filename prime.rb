d95874329cb5cc35bf99a418147febc2a0e9571d
Sean Padden <sean.padden7@gmail.com>
2019-05-02 21:40:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,10 @@
 def prime?(n)
-  for d in 2..(n - 1)
+  if n > 0 
+    one_under = n-1
+  else
+    one_under = n+1
+  end
+  for d in 2..one_under
     if (n % d) == 0
       return false
     end
